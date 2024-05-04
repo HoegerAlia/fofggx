d4d4c897c3e7a647529665ffbb733fdcc3e9c5df
LynchDeontae <114746732+HoegerAlia@users.noreply.github.com>
2024-05-04 10:10:03 +0000

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = '52f9f502-5056-4d42-b3c3-4747732017ae';
 
 // https://www.nslookup.io/domains/cdn.xn--b6gac.eu.org/dns-records/
 // https://www.nslookup.io/domains/cdn-all.xn--b6gac.eu.org/dns-records/
